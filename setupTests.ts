53b866d2150ee21a8d2e4dc4a4ff4b0742285328
nulltoken <emeric.fermas@gmail.com>
2020-03-25 09:23:48 +0100

    fixup! WIP

--- setupTests.ts
+++ setupTests.ts
@@ -3,7 +3,7 @@ import { RulesetExceptionCollection } from './src/types/ruleset';
 import { Dictionary } from '@stoplight/types';
 import { IRunRule, isAsyncApiv2, Rule, Spectral } from './src';
 import { readRuleset } from './src/rulesets';
-import { getDiagnosticSeverity } from './src/rulesets/severity';
+import { asyncApiRules } from './src/rulesets/asyncapi/index.json';
 
 export const buildRulesetExceptionCollectionFrom = (
   loc: string,
@@ -23,20 +23,23 @@ export const buildTestSpectralWithAsyncApiRule = async (ruleName: string): Promi
   s.registerFormat('asyncapi2', isAsyncApiv2);
 
   const dic: Dictionary<Rule, string> = {};
-  const rule = ruleset.rules[ruleName];
-  dic[ruleName] = rule;
+  const rule1 = ruleset.rules[ruleName];
 
-  if (rule.severity === void 0) {
-    throw new Error('Unexpected undefined severity');
-  }
+  const x = asyncApiRules[ruleName];
+  const r: Rule = Object.assign(rule1, {
+    recommended: true,
+    severity: x.severity,
+  });
 
-  const expectedSeverity = getDiagnosticSeverity(rule.severity);
-
-  expect(expectedSeverity).not.toEqual(-1);
-
+  dic[ruleName] = r;
   s.setRules(dic);
 
-  expect(Object.keys(s.rules)).toContain(ruleName);
+  expect(Object.keys(s.rules)).toEqual([ruleName]);
 
-  return [s, s.rules[ruleName]];
+  const rule = s.rules[ruleName];
+  expect(rule.recommended).not.toBe(false);
+  expect(rule.severity).not.toBeUndefined();
+  expect(rule.severity).not.toEqual(-1);
+
+  return [s, rule];
 };
